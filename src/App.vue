57f9404c78791c0f0300a8fc52573de564d203ee
romina de laurentiis <romidelaurentii@gmail.com>
2024-12-03 10:33:46 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template >
+  <div>
+    <h1 class="text-8xl font-black text-emerald-400">{{ title }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data(){
+    return{
+      title: "Hola!🐼​"
+    }
+  }
+}
+</script>
+<style >
+  
+</style>
