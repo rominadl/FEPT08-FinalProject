798c7579d327a21f81b170ea52fee534c82fa22b
romina de laurentiis <romidelaurentii@gmail.com>
2024-12-04 12:10:11 +0100

    inicioprimeroscambios

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,42 @@
-<template >
+
+<template>
   <div>
-    <h1 class="text-8xl font-black text-emerald-400">{{ title }}</h1>
-    <h2 class="text-6xl font-black text-emerald-800">Buen Dia ​🦋​</h2>
-   <RouterView/>
+    <h1 class=""></h1>
+      <nav class="flex border border-e-black justify-between p-5 ">
+        <img src="https://img.freepik.com/vector-gratis/logotipo-abstracto-comida-verde-crema_23-2149153009.jpg?t=st=1733302958~exp=1733306558~hmac=4c253ec9d646ae1a85510d134dbaf40b119097c87ed3b9bdde65cb9ea1386d0d&w=996" alt="Logo" class="logo h-16 w-16 "  />
+        <button class="bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded-full h-16 w-36">
+        INICIAR SESION
+    </button>
+    <button class="bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded-full h-16 w-36">
+        REGISTRATE
+    </button>
+    
+      <ul class="flex justify-evenly">
+        <li><router-link to="/"></router-link></li>
+        <li><router-link to="/Buscador"></router-link></li>
+        <li><router-link to="/MiLista"></router-link></li>
+        <li><router-link to="/MiPerfil"></router-link></li>
+        <li><router-link to="/Kanban"></router-link></li>
+      </ul>
+    </nav>
+    <RouterView/>
   </div>
 </template>
-<script>
-export default {
-  name: "App",
-  data(){
+  <script>
+  export default {
+    
+      name: "Home",
+      data(){
     return{
-      title: "Hola!🐼​"
+      title: "SE EL PROVEEDOR MAS EFICAZ",
+      description: "Busca tu restaurante, busca quien te necesita conecta con tus potenciales clientes"
     }
   }
+  
 }
 </script>
-<style >
+ 
+  <style >
   
-</style>
+      
+  </style>
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,42 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from './views/Home.vue'
+import Buscador from './views/Buscador.vue'
+import MiPerfil from './views/MiPerfil.vue'
+import MiLista from './views/MiLista.vue'
+import Kanban from './views/Kanban.vue'
+import NotFound from './views/NotFound.vue'
 
 const routes = [
+     { path: "/:path(.*)",
+        name:"NotFound",
+        component:NotFound
+    },
+
     { path:"/",
         name:"Home",
         component:Home
-    }
+    },
+
+    { path:"/Buscador",
+        name:"Buscador",
+        component:Buscador
+    },
+
+    { path:"/MiPerfil",
+        name:"MiPerfil",
+        component:MiPerfil
+    },
+
+    { path:"/MiLista",
+        name:"MiLista",
+        component:MiLista
+    },
+
+    { path:"/Kanban",
+        name:"Kanban",
+        component:Kanban
+    },
+
 
 ];
 
