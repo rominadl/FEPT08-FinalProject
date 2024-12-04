154bd62617e1d4124f61e94da8a427347f07dece
Antonia90 <antominnucci@gmail.com>
2024-12-04 13:18:55 +0100

    footer sin estilo

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
-
 <template>
   <div>
     <h1 class=""></h1>
-      <nav class="flex border border-e-black justify-between p-5 ">
-        <img src="https://img.freepik.com/vector-gratis/logotipo-abstracto-comida-verde-crema_23-2149153009.jpg?t=st=1733302958~exp=1733306558~hmac=4c253ec9d646ae1a85510d134dbaf40b119097c87ed3b9bdde65cb9ea1386d0d&w=996" alt="Logo" class="logo h-16 w-16 "  />
-        <button class="bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded-full h-16 w-36">
+    <nav class="flex border border-e-black justify-between p-5">
+      <img
+        src="https://img.freepik.com/vector-gratis/logotipo-abstracto-comida-verde-crema_23-2149153009.jpg?t=st=1733302958~exp=1733306558~hmac=4c253ec9d646ae1a85510d134dbaf40b119097c87ed3b9bdde65cb9ea1386d0d&w=996"
+        alt="Logo"
+        class="logo h-16 w-16"
+      />
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full h-16 w-36"
+      >
         INICIAR SESION!!!
-    </button>
-    <button class="bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded-full h-16 w-36">
+      </button>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full h-16 w-36"
+      >
         REGISTRATE
-    </button>
+      </button>
       <ul class="flex justify-evenly">
         <li><router-link to="/"></router-link></li>
         <li><router-link to="/Buscador">BUSCADOR</router-link></li>
@@ -18,24 +25,26 @@
         <li><router-link to="/Kanban"></router-link></li>
       </ul>
     </nav>
-    <RouterView/>
+    <footer class="flex border border-e-black justify-between justify-items-end p-5">
+      <p>© 2024 bunq</p>
+      <a href="">Términos y Condiciones</a>
+      <a href="">Política de Privacidad</a>
+      <a href="">Configuración de Cookies</a>
+    </footer>
+    <RouterView />
   </div>
 </template>
-  <script>
-  export default {
-    
-      name: "Home",
-      data(){
-    return{
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
       title: "SE EL PROVEEDOR MAS EFICAZ",
-      description: "Busca tu restaurante, busca quien te necesita conecta con tus potenciales clientes"
-    }
-  }
-  
-}
+      description:
+        "Busca tu restaurante, busca quien te necesita conecta con tus potenciales clientes",
+    };
+  },
+};
 </script>
- 
-  <style >
-  
-      
-  </style>
+
+<style></style>
