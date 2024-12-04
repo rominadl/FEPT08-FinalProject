22718af772ad85d638cd9e646e023b72c2b5dad2
Ana <ana@MacBook-Pro-de-Ana.local>
2024-12-04 08:56:36 -0300

    cambio Ana

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
       <nav class="flex border border-e-black justify-between p-5 ">
         <img src="https://img.freepik.com/vector-gratis/logotipo-abstracto-comida-verde-crema_23-2149153009.jpg?t=st=1733302958~exp=1733306558~hmac=4c253ec9d646ae1a85510d134dbaf40b119097c87ed3b9bdde65cb9ea1386d0d&w=996" alt="Logo" class="logo h-16 w-16 "  />
         <button class="bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded-full h-16 w-36">
-        INICIAR SESION
+        INICIAR SESION!!!
     </button>
     <button class="bg-blue-500 hover:bg-blue-700  text-white font-bold py-2 px-4 rounded-full h-16 w-36">
         REGISTRATE
