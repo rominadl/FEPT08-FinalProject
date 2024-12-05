7b6975ac5961a553aa7498caab888f683ec04c76
Antonia90 <antominnucci@gmail.com>
2024-12-05 13:13:25 +0100

    cambios

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
       <button
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full h-16 w-36"
       >
-        INICIAR SESION!!!
+        INICIAR SESION
       </button>
       <button
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full h-16 w-36"
